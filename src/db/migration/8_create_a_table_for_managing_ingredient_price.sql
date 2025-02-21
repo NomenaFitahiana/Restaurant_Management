5713b8930aff91f9110f51c16ea5f78d8e990112
Nomena Fitahiana <nomenafitahiana.randrianirina@gmail.com>
2025-02-21 20:59:47 +0300

    feat (db): create a new table price for managing price

---
+++ src/db/migration/8_create_a_table_for_managing_ingredient_price.sql
@@ -0,0 +1,16 @@
+alter table ingredient drop column unit_price;
+
+create table if not exists price (
+    id int primary key, 
+    unit_price float,
+    montant float,
+    addedon TIMESTAMP,
+);
+
+alter table price add column ingredient_id int;
+
+alter table price add constraint fk_ingredient foreign key (ingredient_id) references ingredient (id) on delete cascade;
+
+alter table price drop column montant;
+
+alter table dish_ingredient add column montant float;
